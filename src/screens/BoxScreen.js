7d41cf4858be99937bfb5bc24a492f7ff339a528
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-08-09 19:37:14 +0500

    BoxScreen created

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View>
+            <Text>Box Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
